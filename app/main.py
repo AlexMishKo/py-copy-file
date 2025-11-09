5df24e1a2bc7ab110ebc56428a2fa451fcd70623
Oleksii Mishchenko <alex3hard@gmail.com>
2025-11-09 20:15:44 +0200

    func created

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    origin_filename = command.split()[1]
+    copy_filename = command.split()[2]
+    if origin_filename == copy_filename:
+        return
+
+    try:
+        with (open(origin_filename, "r") as file_in,
+              open(copy_filename, "w") as file_out):
+            content = file_in.read()
+            file_out.write(content)
+    except FileNotFoundError:
+        pass
